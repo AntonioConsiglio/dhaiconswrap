da804d6df29e20e66075bc97f61e874ee19b4b58
Antonio Consiglio <77753494+AntonioConsiglio@users.noreply.github.com>
2022-10-20 22:38:34 +0200

    package

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='dhaiconswrap',
-    version='0.1.0',    
+    version='0.1.8',    
     description='A depthai wrapper with calibration and points cloud functions',
     url='https://github.com/AntonioConsiglio/dhaiconswrap',
     author='Antonio Consiglio',
     author_email='consiglio.antonio.bi@gmail.com',
-    license='LGPL',
-    packages=['dhaiconswrap'],
+    license='MIT',
+    namespace_packages=['dhaiconswrap'],
+    packages= find_packages(),
     install_requires=['depthai>=2.15.0.0',
-                      'numpy>=1.22.1'
-                      'opencv-python>=4.5.5.64'
+                      'numpy>=1.22.1',
+                      'opencv-python>=4.5.5.64',
                       'opencv-contrib-python>=4.5.5.64',                     
                       ],
 
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research/hobbiest',
-        'License :: LGPL License',  
-        'Operating System :: POSIX :: Linux :: Windows',        
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux', 
+        'Operating System :: Microsoft :: Windows :: Windows 10',       
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
