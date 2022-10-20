55bcc6fdb8527cd716943af339c9e5449ce4c857
Antonio Consiglio <77753494+AntonioConsiglio@users.noreply.github.com>
2022-10-20 19:23:07 +0200

    intro

--- dhaiconswrap/__init__.py
+++ dhaiconswrap/__init__.py
@@ -1,3 +1,12 @@
+"""
+dhaiconswrap.
+
+A python wrapper for OAK-Luxonis cameras.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Antonio Consiglio'
+
 from .cameraManager import *
 from .calibrationLib import *
 from .pointclouds_utils import *
