0d4edc08670cdc7acbd1ed1416a5b5251ad39293
Antonio Consiglio <77753494+AntonioConsiglio@users.noreply.github.com>
2022-10-21 19:12:04 +0200

    0.1.11 version

--- build/lib/dhaiconswrap/__init__.py
+++ build/lib/dhaiconswrap/__init__.py
@@ -4,15 +4,16 @@ dhaiconswrap.
 A python wrapper for OAK-Luxonis cameras.
 Calibration function and Points Cloud manager can be used with other cameras if you use the same method names.
 """
+__version__ = "0.1.11"
+__author__ = "Consiglio Antonio"
 
-__version__ = "0.1.10"
-__author__ = 'Antonio Consiglio'
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+# try:
+#     __import__('pkg_resources').declare_namespace(__name__)
+# except ImportError:
+#     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
     
 from .calibrationLib import docalibration
 from .pointclouds_utils import PointsCloudManager
 from .cameraManager import DeviceManager,get_available_device
+
 
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup,find_packages
 
 setup(
     name='dhaiconswrap',
-    version='0.1.10',    
+    version='0.1.11',    
     description='A depthai wrapper with calibration and points cloud functions',
     url='https://github.com/AntonioConsiglio/dhaiconswrap',
     author='Antonio Consiglio',
     author_email='consiglio.antonio.bi@gmail.com',
     license='MIT',
-    namespace_packages=['dhaiconswrap'],
+    # namespace_packages=['dhaiconswrap'],
     packages= find_packages(),
     install_requires=['depthai>=2.15.0.0',
                       'numpy>=1.22.1',
